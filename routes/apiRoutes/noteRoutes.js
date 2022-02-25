f94cd74d5534031343531d33364aaef45bc2c786
Kelly Lee <amorfati38@gmail.com>
2022-02-24 19:28:11 -0700

    post new note feature

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { notes } = require('../../db/db.json');
 
+router.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
 
-
-
+router.post('/api/notes', (req, res) => {
+    const note
+})
 
 module.exports = router;
